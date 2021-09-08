a328801da0a3ae590242a524aac2db47756e76f2
Jason Francisco Macas Mora <admin@admins-MacBook-Pro.local>
2021-09-08 00:09:48 -0500

    emitter class added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# SIMPLE EVENT EMITTER (SEVENT)
+# Simple Event Emitter
 Simple event emitter without dependencies.
+
+## EXAMPLE
+--------
+```
+from sevent import Emitter 
+import time
+
+def callback(message):
+    print(message)
+
+def do_something():
+    time.sleep(1)
+
+event = Emitter() 
+event.on('ready', callback)
+do_something()
+event.emit('ready', 'Finished!')
+```
+## LICENSE
+--------
+MIT
--- sevent.py
+++ sevent.py
@@ -1,1 +1,47 @@
-print('Event')
+class Emitter:
+    """This class implements a simple event emitter.
+    
+    Example usage::
+
+        from sevent import Emitter 
+        import time
+        
+        def callback(message):
+            print(message)
+        
+        def do_something():
+            time.sleep(1)
+
+        event = Emitter() 
+        event.on('ready', callback)
+        do_something()
+        event.emit('ready', 'Finished!')
+    """
+    def __init__(self, *args, **kwargs):
+        self.callback = None
+        self.callbacks = {}
+
+    def on(self, event_name, callback):
+        """It sets the callback functions.
+
+        :param event_name: str, name of the event
+        :param callback: function
+
+        """
+        if self.callbacks is None:
+            self.callbacks = {}
+        if event_name not in self.callbacks:
+            self.callbacks[event_name] = [callback]
+        else:
+            self.callbacks[event_name].append(callback)
+
+    def emit(self, event_name, *args, **kwargs):
+        """It emits an event, and calls the corresponding callback function
+        :param event_name: str, name of the event.
+        """
+        if self.callbacks is not None and event_name in self.callbacks:
+            for callback in self.callbacks[event_name]:
+                if callback.__code__.co_argcount > 0:
+                    callback(*args, **kwargs)
+                else:
+                    callback()
