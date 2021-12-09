de0a5f872ce5220fa3412aabe28e89bc6d6218c0
Jason Francisco Macas Mora <admin@admins-MacBook-Pro.local>
2021-12-08 22:17:06 -0500

    clear event method added

--- sevent.py
+++ sevent.py
@@ -3,36 +3,45 @@ class Emitter:
     
     Example usage::
 
+        callback = lambda message: print(message)
+
         event = Emitter() 
         event.on('ready', callback)
-        do_something()
         event.emit('ready', 'Finished!')
     """
     def __init__(self, *args, **kwargs):
         self.callback = None
         self.callbacks = {}
 
-    def on(self, event_name, callback):
+    def on(self, eventName:str, callback):
         """It sets the callback functions.
 
-        :param event_name: str, name of the event
+        :param eventName: name of the event
         :param callback: function
-
         """
         if self.callbacks is None:
             self.callbacks = {}
-        if event_name not in self.callbacks:
-            self.callbacks[event_name] = [callback]
+        if eventName not in self.callbacks:
+            self.callbacks[eventName] = [callback]
         else:
-            self.callbacks[event_name].append(callback)
+            self.callbacks[eventName].append(callback)
 
-    def emit(self, event_name, *args, **kwargs):
+    def emit(self, eventName:str, *args, **kwargs):
         """It emits an event, and calls the corresponding callback function.
-        :param event_name: str, name of the event.
+
+        :param eventName: name of the event.
         """
-        if self.callbacks is not None and event_name in self.callbacks:
-            for callback in self.callbacks[event_name]:
+        if self.callbacks is not None and eventName in self.callbacks:
+            for callback in self.callbacks[eventName]:
                 if callback.__code__.co_argcount > 0:
                     callback(*args, **kwargs)
                 else:
                     callback()
+
+    def clearEvent(self, eventName:str):
+        """It clears the callbacks associated to a specific event name.
+
+        :param eventName: name of the event.
+        """
+        if eventName in self.callbacks:
+            del self.callbacks[eventName]
